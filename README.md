709e4ac5285960432a9c5af1618af1621e4eb5ee
Nikolay Rangelov <nikolay.a.rangelov@mail.bg>
2017-01-29 15:36:36 +0200

    Filter added

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # betterfy-web
 Betterfy is an open source application for tracking habits and managing tasks.
+
+#Endpoints
+
+GET - http://localhost:8080/api/test : test endpoint just to verify if Jersey is working
+
+#Authentication
+When the application receives the token it should send it in this format:
+
+HEADER : VALUE
+USER_ID : token
---
+++ betterfy/src/main/java/betterfy/filter/SecurityFilter.java
@@ -0,0 +1,69 @@
+package betterfy.filter;
+
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Priority;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.Priorities;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+
+
+/**
+ * Created by nik on 1/29/17.
+ */
+
+@Component
+@Priority(Priorities.AUTHENTICATION)
+public class SecurityFilter implements Filter {
+
+    public static final String USER_ID_HEADER = "USER_ID";
+    private static final String SECURED_URL_PREFIX = "secured";
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest)req;
+        HttpServletResponse response = (HttpServletResponse) res;
+        String authToken = null;
+
+        String requestedUrl = request.getRequestURI().toString();
+        authToken = request.getHeader(USER_ID_HEADER);
+        System.out.println(authToken);
+
+        if(requestedUrl.contains(SECURED_URL_PREFIX)){
+
+            if(!isTokenValid(authToken)){
+                Response.ResponseBuilder builder = null;
+                String msg = "User cannot access the resource!";
+                builder = Response.status(Response.Status.UNAUTHORIZED).entity(msg);
+                throw new WebApplicationException(builder.build());
+            }
+
+
+
+        }
+        //response.setHeader(USER_ID_HEADER, "12");
+        chain.doFilter(req, res);
+    }
+
+
+    private boolean isTokenValid(String token) {
+
+        return true;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+}
